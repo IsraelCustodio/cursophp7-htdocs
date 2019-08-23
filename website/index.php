c1c188ef7e71fee462d23f394bcf176178881b7e
Israel Custódio <israelbianca@gmail.com>
2019-08-23 11:29:44 -0300

    Sudindo os arquivos da aula sobre Slim Framework

--- website/index.php
+++ website/index.php
@@ -1,5 +1,21 @@
 <?php
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+// Isso são rotas em Slim Framework
+$app->get('/', function(){
+    echo json_encode(array(
+        'date' => date("Y-m-d H:i:s")
+    ));
+});
+
+// Isso são rotas em Slim Framework
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
 
  ?>
