394c196d0506fcd17d8f06c3cb7ceeb423b5af8a
Israel Custódio <israelbianca@gmail.com>
2019-08-27 11:38:23 -0300

    Subindo mais algumas alterações com relação as aulas de segurança.

--- seguranca/exemplo-04.php
+++ seguranca/exemplo-04.php
@@ -3,3 +3,12 @@
 
 
  ?>
+
+<script src="https://www.google.com/recaptcha/api.js"></script>
+<form action="cadastrar.php" method="post">
+
+    <div class="g-recaptcha" data-sitekey="6LfDKrUUAAAAAF7878aRl88KF-_mVA_1UJ-XqTCH"></div>
+    <input type="email" name="inputEmail">
+    <button type="submit">Enviar</button>
+
+</form>
